1e40e2c259eab665618eb87a737bd019e058340c
Vineet Singh <vineetdsat@gmail.com>
2022-01-16 19:00:44 +0530

    Added Population Effected Query

--- Data Exploration.sql
+++ Data Exploration.sql
@@ -8,8 +8,15 @@ SELECT Location, date, total_cases, total_deaths, population
 FROM Covid..CovidDeaths$
 order by 1, 2
 
+
+-- Query for Getting the Death Percentage
 SELECT Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 FROM Covid..CovidDeaths$
 WHERE Location = 'India'
 order by 1, 2
 
+-- Query for Percentage of people got Covid
+SELECT Location, date, total_cases, (total_cases/population)*100 as PopulationEffected
+FROM Covid..CovidDeaths$
+WHERE Location = 'India'
+order by 1, 2
